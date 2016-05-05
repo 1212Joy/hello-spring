92e52c8b13ab64e943a5e99f4b4752984ad8708b
zhaijiayi <zhaijiayi@fero.com.cn>
2016-05-05 10:15:15 +0800

    #add mybatis

--- src/test/java/cn/com/basic/SimpleTest.java
+++ src/test/java/cn/com/basic/SimpleTest.java
@@ -9,6 +9,7 @@ public class SimpleTest {
 
     @Test
     public void randomDouble() throws Exception {
+        
         java.util.Random random=new java.util.Random();
         int randomCode = random.nextInt(99)+1;
         double a = randomCode*0.01;
