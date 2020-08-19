143eec47c701fd787e92dbe28f9d1e6961f87c1b
zhaijiayi <zhaijiayi@maoyan.com>
2020-08-19 19:30:28 +0800

    leetcode

--- src/test/java/cn/com/basic/leetcode/ArrayleetcodeTest.java
+++ src/test/java/cn/com/basic/leetcode/ArrayleetcodeTest.java
@@ -12,7 +12,8 @@ public class ArrayleetcodeTest {
 
     @Test
     public void test() throws Exception {
-
+        int[] array = {10, 9, 2, 5, 3, 7, 101, 18};
+        lengthOfLIS(array);
     }
 
     /**
@@ -538,5 +539,32 @@ public class ArrayleetcodeTest {
         return sum;
     }
 
+
+    /**
+     * todo 300. 最长上升子序列
+     * 暴利求解： O(n2) - 动态规划
+     * 优化算法：O(n log n) - 动态规划 + 二分查找
+     *
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        int res = 0;
+        //这个方法啥意思，将dp数组内的每一个索引位置都 设置成1，但是为啥都是1呢？
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                //todo 这块为啥啊啊啊
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+
+    }
 
 }
